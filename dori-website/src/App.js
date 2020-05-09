0a83ad361eeb0b3c95fa77cf08185dc95f0d7686
Asia Szpadkiewicz <joanna.szpadkiewicz@gmail.com>
2020-05-09 22:11:15 +0200

    blank webstie scheme

---
+++ dori-website/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style/Main.scss';
+
+function App() {
+    return (
+        <div className="App">
+        <header className="App-header">
+
+        </header>
+        </div>
+);
+}
+
+export default App;
+
