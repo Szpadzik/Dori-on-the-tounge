38d2a255b10f0aad363430f84662db4dc27cd4de
Asia Szpadkiewicz <joanna.szpadkiewicz@gmail.com>
2020-05-15 09:58:07 +0200

    AboutMe Section is done

---
+++ dori-website/src/components/AboutMe.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+//import Link from 'react-scroll';
+
+
+function AboutMe() {
+
+    return (
+        <div className="container">
+            <div className="about_me">
+                <div className="about_photo">
+                    <div className="about_image"></div>
+                    <div className="about_frame"></div>
+                    <p className="about_tag">teaching is my superpower</p>
+                </div>
+                <div className="about_text">
+                    <div className="about_me_box">
+                        <h3>About Me</h3>
+                        <h4>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            nimi aspernatur atque dolorum fuga impedit ipsa laboriosam quia quisquam soluta
+                            voluptatibus.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            nimi aspernatur atque dolorum fuga impedit ipsa laboriosam quia quisquam soluta
+                            voluptatibus.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            nimi aspernatur atque dolorum fuga impedit ipsa laboriosam quia quisquam soluta
+                            voluptatibus.
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AboutMe;
--- dori-website/src/components/IntroSection.js
+++ dori-website/src/components/IntroSection.js
@@ -7,7 +7,11 @@ function Intro() {
         <div className="container">
             <div className="intro">
                 <div className="description_box">
-                    <p className="description_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, ipsa.</p>
+                    <h3> why are you here ? </h3>
+                    <p className="description_text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam asperiores
+                        aspernatur atque commodi culpa dolorem, eaque, enim est eum, explicabo laudantium molestiae
+                        nam praesentium provident quo sequi. Aut, possimus!</p>
                 </div>
                 <div className="banner_box">
                     <p className="welcome_text">How are You?</p>
